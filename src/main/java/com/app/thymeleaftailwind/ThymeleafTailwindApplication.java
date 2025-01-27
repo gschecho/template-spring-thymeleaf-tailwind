562bc06e54792c46bc4f7f4b264acfe57546c012
Gonzalo Scarpa <gschecho@gmail.com>
2025-01-27 23:15:27 +0100

    first commit

---
+++ src/main/java/com/app/thymeleaftailwind/ThymeleafTailwindApplication.java
@@ -0,0 +1,13 @@
+package com.app.thymeleaftailwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTailwindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafTailwindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/thymeleaftailwind/ThymeleafTailwindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.thymeleaftailwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafTailwindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
